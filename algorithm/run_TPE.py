013323a7de7a80b1c04d973fda272ffafd17773b
Rui Shu <terry.shu@hotmail.com>
2019-10-05 00:59:04 -0400

    add tpe

--- algorithm/run_TPE.py
+++ algorithm/run_TPE.py
@@ -1,5 +1,122 @@
+import csv
+import numpy as np
+import pandas as pd
+from hyperopt import STATUS_OK
+from timeit import default_timer as timer
+import lightgbm as lgb
+from hyperopt import tpe
+from hyperopt import Trials
+from hyperopt import fmin
+from pathlib import Path
+import ast
+from sklearn.metrics import roc_auc_score
+
+from util import readData
+from parameterGrid import get_tpe_parameter
+
+MAX_EVALS = 10
+N_FOLDS = 10
+base_path = Path(__file__).parent
+out_file = (base_path / "../results/gbm_trials.csv").resolve()
 
 
+def tpe_objective(params, n_folds=N_FOLDS):
+    """Objective function for Gradient Boosting Machine Hyperparameter Optimization"""
+
+    # Keep track of evals
+    global ITERATION
+    ITERATION += 1
+
+    # Retrieve the subsample if present otherwise set to 1.0
+    subsample = params['boosting_type'].get('subsample', 1.0)
+
+    # Extract the boosting type
+    params['boosting_type'] = params['boosting_type']['boosting_type']
+    params['subsample'] = subsample
+
+    # Make sure parameters that need to be integers are integers
+    for parameter_name in ['num_leaves', 'subsample_for_bin', 'min_child_samples']:
+        params[parameter_name] = int(params[parameter_name])
+
+    start = timer()
+
+    # Perform n_folds cross validation
+    cv_results = lgb.cv(params, train_set, num_boost_round=10000, nfold=n_folds,
+                        early_stopping_rounds=100, metrics='auc', seed=50)
+
+    run_time = timer() - start
+
+    best_score = np.max(cv_results['auc-mean'])
+    loss = 1 - best_score
+
+    # Boosting rounds that returned the highest cv score
+    n_estimators = int(np.argmax(cv_results['auc-mean']) + 1)
+
+    # Write to the csv file ('a' means append)
+    of_connection = open(out_file, 'a')
+    writer = csv.writer(of_connection)
+    writer.writerow([loss, params, ITERATION, n_estimators, run_time])
+
+    # Dictionary with information for evaluation
+    return {'loss': loss, 'params': params, 'iteration': ITERATION,
+            'estimators': n_estimators,
+            'train_time': run_time, 'status': STATUS_OK}
+
+
+# Tree Parzen Estimator (TPE) is a Bayesian Optimization
 def run_TPE(dataset):
+    params = get_tpe_parameter()
+    train_features, test_features, train_label, test_label = readData.prepareData(dataset)
 
-    pass
+    global train_set
+    # Create a lgb dataset
+    train_set = lgb.Dataset(train_features, label=train_label)
+
+    # # optimization algorithm
+    # tpe_algorithm = tpe.suggest
+
+    # Keep track of results
+    bayes_trials = Trials()
+
+    of_connection = open(out_file, 'w')
+    writer = csv.writer(of_connection)
+
+    # Write the headers to the file
+    writer.writerow(['loss', 'params', 'iteration', 'estimators', 'train_time'])
+    of_connection.close()
+
+    # Global variable
+    global ITERATION
+
+    ITERATION = 0
+
+    # Run optimization
+    best = fmin(fn=tpe_objective, space=params, algo=tpe.suggest,
+                max_evals=MAX_EVALS, trials=bayes_trials, rstate=np.random.RandomState(50))
+
+    # # Sort the trials with lowest loss (highest AUC) first
+    # bayes_trials_results = sorted(bayes_trials.results, key=lambda x: x['loss'])
+    # print(bayes_trials_results)
+
+    results = pd.read_csv(out_file)
+
+    # Sort with best scores on top and reset index for slicing
+    results.sort_values('loss', ascending=True, inplace=True)
+    results.reset_index(inplace=True, drop=True)
+
+    # Extract the ideal number of estimators and hyperparameters
+    best_bayes_estimators = int(results.loc[0, 'estimators'])
+
+    # Convert from a string to a dictionary
+    best_bayes_params = ast.literal_eval(results.loc[0, 'params']).copy()
+
+    # Re-create the best model and train on the training data
+    best_bayes_model = lgb.LGBMClassifier(n_estimators=best_bayes_estimators, n_jobs=-1,
+                                          objective='binary', random_state=50, **best_bayes_params)
+    best_bayes_model.fit(train_features, train_label)
+
+    # Evaluate on the testing data
+    preds = best_bayes_model.predict_proba(test_features)[:, 1]
+    print('The best model from Bayes optimization scores {:.5f} AUC ROC on the test set.'.format(
+        roc_auc_score(test_label, preds)))
+    print('This was achieved after {} search iterations'.format(results.loc[0, 'iteration']))
--- util/parameterGrid.py
+++ util/parameterGrid.py
@@ -1,7 +1,9 @@
 import numpy as np
 import random
+from hyperopt import hp
+from hyperopt.pyll.stochastic import sample
 
-# Hyperparameter grid
+# Hyperparameter grid for random search
 param_grid = {
     'class_weight': [None, 'balanced'],
     'boosting_type': ['gbdt', 'goss', 'dart'],
@@ -21,4 +23,31 @@ subsample_dist = list(np.linspace(0.5, 1, 100))
 def get_parameters():
     params = {key: random.sample(value, 1)[0] for key, value in param_grid.items()}
     params['subsample'] = random.sample(subsample_dist, 1)[0] if params['boosting_type'] != 'goss' else 1.0
+    return params
+
+
+# In Hyperopt, and other Bayesian optimization frameworks,
+# the domian is not a discrete grid but instead has probability
+# distributions for each hyperparameter.
+
+# Define the search space
+tpe_grid = {
+    'class_weight': hp.choice('class_weight', [None, 'balanced']),
+    'boosting_type': hp.choice('boosting_type',
+                               [{'boosting_type': 'gbdt', 'subsample': hp.uniform('gdbt_subsample', 0.5, 1)},
+                                {'boosting_type': 'dart', 'subsample': hp.uniform('dart_subsample', 0.5, 1)},
+                                {'boosting_type': 'goss', 'subsample': 1.0}]),
+    'num_leaves': hp.quniform('num_leaves', 30, 150, 1),
+    'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(0.2)),
+    'subsample_for_bin': hp.quniform('subsample_for_bin', 20000, 300000, 20000),
+    'min_child_samples': hp.quniform('min_child_samples', 20, 500, 5),
+    'reg_alpha': hp.uniform('reg_alpha', 0.0, 1.0),
+    'reg_lambda': hp.uniform('reg_lambda', 0.0, 1.0),
+    'colsample_bytree': hp.uniform('colsample_by_tree', 0.6, 1.0)
+}
+
+
+def get_tpe_parameter():
+    # Sample from the full space
+    params = sample(tpe_grid)
     return params
