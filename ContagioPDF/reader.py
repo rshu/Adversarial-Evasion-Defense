1ccbbe33e2c8594e22af4ebecc0ca18d2f3ac668
Rui Shu <terry.shu@hotmail.com>
2020-04-20 23:09:06 -0400

    contagio pdf acc 99.66%

---
+++ ContagioPDF/reader.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import tensorflow as tf
+import keras
+from os import path
+import pickle
+from keras.layers import Input, Dense, Dropout, Activation
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import numpy as np
+
+desired_width = 320
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 50)
+
+
+def load_dataset(file_path):
+    df = pd.read_csv(file_path, encoding='utf8', low_memory=False)
+    df = df.drop(['filename'], axis=1)
+    df.columns = df.columns.str.lstrip()
+
+    print(df.shape) # (27205, 136)
+    print(df.dtypes)
+    print(df.head())
+    print(df.describe())
+    # print(list(df.Label.unique()))
+    print(df['class'].value_counts())
+    print(df['class'].isnull().sum())
+
+    df = df.dropna()
+    df = df.reset_index(drop=True)
+    print(df.shape)  # (27205, 136)
+
+    print(df.select_dtypes(exclude=['int', 'float']))
+
+    df['class'] = df['class'].astype(str).map({'False': 0, 'True': 1})
+    df['box_other_only'] = df['box_other_only'].astype(str).map({'False': 0, 'True': 1})
+    df['pdfid_mismatch'] = df['pdfid_mismatch'].astype(str).map({'False': 0, 'True': 1})
+
+    # print(df.select_dtypes(exclude=['int', 'float']))
+    print(df['class'].value_counts())
+    print(df['class'].dtypes)
+    print(df['box_other_only'].dtypes)
+    print(df['pdfid_mismatch'].dtypes)
+
+    df = df.drop_duplicates()
+    print(df.shape) # (22525, 136)
+
+    df.to_pickle("./saved_dataframe.pkl")
+    return df
+
+def build_models(X_train, y_train, X_test, y_test):
+    config = tf.compat.v1.ConfigProto(device_count={'GPU': 1, 'CPU': 8})
+    sess = tf.compat.v1.Session(config=config)
+    tf.compat.v1.keras.backend.set_session(sess)
+
+    # dropout to avoid overfitting
+    layers = [
+        Dense(X_train.shape[1], input_shape=(X_train.shape[1],)),
+        Activation('relu'),
+        # Dropout(0.5),
+        Dense(64),
+        Activation('relu'),
+        # Dropout(0.5),
+        Dense(32),
+        Activation('relu'),
+        # Dropout(0.5),
+        Dense(16),
+        Activation('relu'),
+        # Dropout(0.5),
+        Dense(1),
+        Activation('sigmoid')
+    ]
+
+    classifier = keras.Sequential()
+    for layer in layers:
+        classifier.add(layer)
+
+    classifier.compile(optimizer='adam',
+                       loss='binary_crossentropy',
+                       metrics=['accuracy'])
+    classifier.fit(X_train, y_train, batch_size=32, epochs=10)
+
+    # save the model
+    nn_model_pickel_file = 'saved_nn_model.pkl'
+    pickle.dump(classifier, open(nn_model_pickel_file, 'wb'))
+
+    # load the model from disk
+    # loaded_model = pickle.load(open(nn_model_pickel_file, 'rb'))
+
+    print("evaluation...")
+    print("")
+    print(classifier.evaluate(X_test, y_test, verbose=2))
+
+
+if __name__ == "__main__":
+    file_path = "../data/ContagioPDF/ConsolidateData.csv"
+
+    if path.exists("saved_dataframe.pkl"):
+        df = pd.read_pickle("./saved_dataframe.pkl")
+    else:
+        df = load_dataset(file_path)
+
+    X = df.drop(['class'], axis=1)
+    y = df['class']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
+
+    scaler1 = preprocessing.StandardScaler().fit(X_train)
+    X_train = scaler1.transform(X_train)
+
+    scaler5 = preprocessing.StandardScaler().fit(X_test)
+    X_test = scaler5.transform(X_test)
+
+    build_models(X_train, y_train, X_test, y_test)
