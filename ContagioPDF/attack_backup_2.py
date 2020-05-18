1d62caa2479910eaa96f83be4faa2663ff3af4c4
Rui Shu <terry.shu@hotmail.com>
2020-05-18 02:02:37 -0400

    preliminary version of deepfool

--- ContagioPDF/attack_backup_2.py
+++ ContagioPDF/attack_backup_2.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow.compat.v1 as tfc
 import keras
 from os import path
 import pickle, sys
@@ -10,8 +11,14 @@ import random
 import pandas as pd
 import csv
 from find_two_models import dnn_model
+import foolbox
+
+from keras import backend as K
+from cleverhans.utils_keras import KerasModelWrapper
+from cleverhans.attacks import CarliniWagnerL2, BasicIterativeMethod, DeepFool, SaliencyMapMethod
 
 file_path = "../data/ContagioPDF/ConsolidateData.csv"
+random.seed(42)
 
 
 def load_dataset(file_path):
@@ -163,7 +170,8 @@ def bim_a_attack(model, X_input, y_input, epsilon=0.2, clip_min=0.0, clip_max=1.
             signed_grad = adversarial_pattern(X_input_each, y_input_each, model)
 
             # the problem is that X_train_each_adv might be the same every time
-            X_input_each_adv = X_input_each + signed_grad * epsilon
+            # add a small noise
+            X_input_each_adv = X_input_each + signed_grad * epsilon + random.uniform(-0.5, 0.5)
 
             # clip function
             X_input_each_adv = tf.clip_by_value(X_input_each_adv, clip_min, clip_max)
@@ -190,7 +198,8 @@ def bim_b_attack(model, X_input, y_input, epsilon=0.2, clip_min=0.0, clip_max=1.
             signed_grad = adversarial_pattern(X_input_each, y_input_each, model)
 
             # the problem is that X_train_each_adv might be the same every time
-            X_input_each_adv = X_input_each + signed_grad * epsilon
+            # add a small noise
+            X_input_each_adv = X_input_each + signed_grad * epsilon + random.uniform(-0.5, 0.5)
             # clip function
             X_input_each_adv = tf.clip_by_value(X_input_each_adv, clip_min, clip_max)
 
@@ -208,8 +217,125 @@ def jsma_attack():
     pass
 
 
-def deepfool_attack():
-    pass
+def deepfool_attack(model, X_input, eta=0.02, epochs=3, clip_min=0.0, clip_max=1.0, min_prob=0.0):
+    '''
+    The original DeepFool will stop whenever we successfully cross the decision boundary.
+
+    '''
+    # Any operation that is being done inside tf.stop_gradient will not be updated during backpropogation.
+    # Here we force deepfool to run full epaches.
+
+    # print(X_input.shape)
+    # y = tf.stop_gradient(model.predict(X_input))
+    # print(y)
+    # print(y.shape)
+
+    batch_size = X_input.shape[0]
+
+    for i in range(batch_size):
+        print(i)
+        X_input_each = X_input[i].reshape((1, X_input.shape[1]))
+
+        delta = _deepfool2(model, X_input_each, eta=eta, epochs=epochs, clip_min=clip_min,
+                           clip_max=clip_max)
+
+        X_adv = tf.stop_gradient(X_input_each + delta * (1 + eta))
+        X_adv = tf.clip_by_value(X_adv, clip_min, clip_max)
+
+        X_input[i] = X_adv
+
+    return X_input
+
+
+# def _prod(iterable):
+#     ret = 1
+#     for x in iterable:
+#         ret *= x
+#     return ret
+
+
+@tf.function
+def adversarial_gradient(X_train_each, y_train_each, input_model):
+    X_train_each = tf.cast(X_train_each, tf.float32)
+
+    with tf.GradientTape() as tape:
+        tape.watch(X_train_each)
+        prediction = input_model(X_train_each)
+        loss = tf.keras.losses.binary_crossentropy(y_train_each, prediction)
+
+    gradient = tape.gradient(loss, X_train_each)
+    return gradient
+
+def _deepfool2(model, X_input_each, eta, epochs, clip_min, clip_max):
+    """DeepFool for binary classifiers.
+    Note that DeepFools that binary classifier outputs +1/-1 instead of 0/1.
+    """
+
+    y0 = tf.stop_gradient(tf.reshape(model.predict(X_input_each), [-1])[0])
+    y0 = tf.cast(tf.greater(y0, 0.0), tf.int32)
+
+    def _cond(i, z):
+        xadv = tf.clip_by_value(X_input_each + z * (1 + eta), clip_min, clip_max)
+        y = tf.stop_gradient(tf.reshape(model.predict(xadv.numpy()), [-1])[0])
+        # y = tf.to_int32(tf.greater(y, 0.0))
+        y = tf.cast(tf.greater(y, 0.0), tf.int32)
+        return tf.logical_and(tf.less(i, epochs), tf.equal(y0, y))
+
+    def _body(i, z):
+        xadv_each = tf.clip_by_value(X_input_each + z * (1 + eta), clip_min, clip_max).numpy()
+        y = 0 - model.predict(xadv_each)[0]
+        y = tf.reshape(y, [-1]).numpy().reshape(1, 1)
+        # y = tf.cast(tf.greater(y, 0.0), tf.int32).numpy().reshape(1, 1)
+
+        # y = tf.reshape(model.predict(xadv_each.numpy()), [-1])
+
+        g = adversarial_gradient(xadv_each,y, model)
+        # issue here
+        # g = tape.gradient(loss, xadv_each)
+
+        # g = tf.gradients(y, xadv)[0]
+
+        dx = - y * g / (tf.norm(g) + 1e-10)  # off by a factor of 1/norm(g)
+        new = z.numpy() + dx.numpy()
+        return i + 1, tf.convert_to_tensor(new) # issue here
+
+    _, noise = tf.while_loop(_cond, _body, [0, tf.zeros_like(X_input_each)],
+                             name='_deepfool2', back_prop=False)
+
+    return noise
+
+    #
+    # xshape = X_input.shape
+    # dim = _prod(xshape)
+    #
+    # print(dim)
+    # print("here")
+    #
+    # def _cond(i, z):
+    #     return tf.less(i, epochs)
+    #
+    # # check this function
+    # def _body(i, z):
+    #     xadv_each = tf.clip_by_value(X_input + z * (1 + eta), clip_min, clip_max)
+    #     y = model.predict(xadv_each.numpy()).reshape(1, 1)
+    #     y = tf.reshape(y, [-1])[0]
+    #
+    #     # y = tf.reshape(model.predict(xadv_each.numpy()), [-1])
+    #
+    #     with tf.GradientTape() as tape:
+    #         tape.watch(xadv_each)
+    #
+    #     g = tape.gradient(y, xadv_each)
+    #
+    #     n = tf.norm(tf.reshape(g, [-1, dim]), axis=1) + 1e-10
+    #     d = tf.reshape(-y / n, [-1] + [1] * len(xshape))
+    #     dx = g * d
+    #     return i + 1, z + dx
+    #
+    # # condition, body, loop variable,
+    # _, noise = tf.while_loop(_cond, _body, [0, tf.zeros_like(X_input)],
+    #                          name='_deepfool2_batch', back_prop=False)
+    # return noise
 
 
 def c_and_w_attack():
@@ -281,35 +407,44 @@ if __name__ == "__main__":
     X_test_bim_b = X_test.copy()
     y_test_bim_b = y_test.copy()
 
-    # fgsm attack on testing dataset
-    X_test_adv_fgsm, y_test_adv_fgsm = fgsm_attack(best_model, X_test_fgsm, y_test_fgsm, epsilon=0.2, clip_min=0.0,
-                                                   clip_max=1.0)
+    X_test_deepfool = X_test.copy()
+    y_test_deepfool = y_test.copy()
 
-    print("Base accuracy of fgsm on best model:", best_model.evaluate(x=X_test_adv_fgsm, y=y_test_adv_fgsm, verbose=0))
-    print("Base accuracy of fgsm on candidate model:",
-          candidate_model.evaluate(x=X_test_adv_fgsm, y=y_test_adv_fgsm, verbose=0))
-    print("")
+    # # fgsm attack on testing dataset
+    # X_test_adv_fgsm, y_test_adv_fgsm = fgsm_attack(best_model, X_test_fgsm, y_test_fgsm, epsilon=0.2, clip_min=0.0,
+    #                                                clip_max=1.0)
+    #
+    # print("Base accuracy of fgsm on best model:", best_model.evaluate(x=X_test_adv_fgsm, y=y_test_adv_fgsm, verbose=0))
+    # print("Base accuracy of fgsm on candidate model:",
+    #       candidate_model.evaluate(x=X_test_adv_fgsm, y=y_test_adv_fgsm, verbose=0))
+    # print("")
+    #
+    # # BIM A attack
+    # X_test_adv_bim_a, y_test_adv_bim_a = bim_a_attack(best_model, X_test_bim_a, y_test_bim_a, epsilon=0.2, clip_min=0.0,
+    #                                                   clip_max=1.0, iterations=10)
+    #
+    # print("Base accuracy of BIM A on best model:",
+    #       best_model.evaluate(x=X_test_adv_bim_a, y=y_test_adv_bim_a, verbose=0))
+    # print("Base accuracy of BIM A on candidate model:",
+    #       candidate_model.evaluate(x=X_test_adv_bim_a, y=y_test_adv_bim_a, verbose=0))
+    # print("")
+    #
+    # # BIM B attack
+    # X_test_adv_bim_b, y_test_adv_bim_b = bim_b_attack(best_model, X_test_bim_b, y_test_bim_b, epsilon=0.2, clip_min=0.0,
+    #                                                   clip_max=1.0, iterations=10)
+    #
+    # print("Base accuracy of BIM B on best model:",
+    #       best_model.evaluate(x=X_test_adv_bim_b, y=y_test_adv_bim_b, verbose=0))
+    # print("Base accuracy of BIM B on candidate model:",
+    #       candidate_model.evaluate(x=X_test_adv_bim_b, y=y_test_adv_bim_b, verbose=0))
 
-    # BIM A attack
-    X_test_adv_bim_a, y_test_adv_bim_a = bim_a_attack(best_model, X_test_bim_a, y_test_bim_a, epsilon=0.2, clip_min=0.0,
-                                                      clip_max=1.0, iterations=10)
+    # print(np.array_equal(X_test, X_test_adv_fgsm))
+    # print(np.array_equal(X_test_adv_fgsm, X_test_adv_bim_a))
+    # print(np.array_equal(X_test_adv_fgsm, X_test_adv_bim_b))
+    # print(np.array_equal(X_test_adv_bim_a, X_test_adv_bim_b))
 
-    print("Base accuracy of BIM A on best model:",
-          best_model.evaluate(x=X_test_adv_bim_a, y=y_test_adv_bim_a, verbose=0))
-    print("Base accuracy of BIM A on candidate model:",
-          candidate_model.evaluate(x=X_test_adv_bim_a, y=y_test_adv_bim_a, verbose=0))
-    print("")
-
-    # BIM B attack
-    X_test_adv_bim_b, y_test_adv_bim_b = bim_b_attack(best_model, X_test_bim_b, y_test_bim_b, epsilon=0.2, clip_min=0.0,
-                                                      clip_max=1.0, iterations=10)
-
-    print("Base accuracy of BIM B on best model:",
-          best_model.evaluate(x=X_test_adv_bim_b, y=y_test_adv_bim_b, verbose=0))
-    print("Base accuracy of BIM B on candidate model:",
-          candidate_model.evaluate(x=X_test_adv_bim_b, y=y_test_adv_bim_b, verbose=0))
-
-    print(np.array_equal(X_test, X_test_adv_fgsm))
-    print(np.array_equal(X_test_adv_fgsm, X_test_adv_bim_a))
-    print(np.array_equal(X_test_adv_fgsm, X_test_adv_bim_b))
-    print(np.array_equal(X_test_adv_bim_a, X_test_adv_bim_b))
+    # deepfool attack
+    X_test_adv_deepfool = deepfool_attack(best_model, X_test_deepfool)
+    print("Done")
+    print("Base accuracy of deepfool on best model:",
+          best_model.evaluate(x=X_test_adv_deepfool, y=y_test_deepfool, verbose=0))
---
+++ ContagioPDF/test_gower_distance.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import gower
+
+desired_width = 320
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 50)
+
+# Xd = pd.DataFrame({'age': [21, 21, 19, 30, 21, 21, 19, 30],
+#                    'gender': ['M', 'M', 'M', 'M', 'F', 'F', 'F', 'F'],
+#                    'civil_status': ['MARRIED', 'SINGLE', 'SINGLE', 'SINGLE', 'MARRIED', 'SINGLE', 'WIDOW', 'DIVORCED'],
+#                    'salary': [3000.0, 1200.0, 32000.0, 1800.0, 2900.0, 1100.0, 10000.0, 1500.0],
+#                    'has_children': [1, 0, 1, 1, 1, 0, 0, 1],
+#                    'available_credit': [2200, 100, 22000, 1100, 2000, 100, 6000, 2200]})
+# Yd = Xd.iloc[0:1, :]
+#
+# print(gower.gower_matrix(Xd, Yd, cat_features=[False, True, True, False, False, False]))
+
+df = pd.read_csv('dl_trials_1.csv')
+# print(df.shape)
+
+# Sort with best scores on top and reset index for slicing
+df.sort_values('loss', ascending=True, inplace=True)
+df.reset_index(inplace=True, drop=True)
+
+print(df)
+print(df.shape)
+
+data = []
+
+for i in range(df.shape[0]):
+    params_dict = eval(df.iloc[i]['params'])
+    # print(params_dict)
+    tmp_list = []
+    tmp_list.append(params_dict['drop_out'])
+    tmp_list.append(params_dict['first_layer_dense'])
+    tmp_list.append(params_dict['hidden_layer_activation'])
+    tmp_list.append(params_dict['optimizer'])
+    tmp_list.append(params_dict['output_layer_activation'])
+    tmp_list.append(params_dict['second_layer_dense'])
+    tmp_list.append(params_dict['third_layer_dense'])
+    # print(tmp_list)
+    data.append(tmp_list)
+
+print(data)
+
+params_dataframe = pd.DataFrame(data, columns=['drop_out', 'first_layer_dense', 'hidden_layer_activation', 'optimizer',
+                                               'output_layer_activation', 'second_layer_dense', 'third_layer_dense'])
+
+print(params_dataframe)
+
+loss_epsilon = 0.1
+best_result = df.iloc[0]
+
+for i in range(df.shape[0]):
+    if df.iloc[i]['loss'] - best_result['loss'] <= loss_epsilon:
+        # print(i)
+        candidate_params = params_dataframe.iloc[i:i+1, :]
+        print(gower.gower_matrix(params_dataframe, candidate_params, cat_features=[False, False, True, True, True, False, False])[0][0])
+
+
+
+
+# Yd = params_dataframe.iloc[1:2, :]
+# print(gower.gower_matrix(params_dataframe, Yd, cat_features=[False, False, True, True, True, False, False]))
+
